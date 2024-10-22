772dd39b486442cae953ea8d5bf35d5f1c944d2a
Giuliana Dealbera Etchechoury <giulydealbera@gmail.com>
2024-10-22 17:04:57 -0300

    modified: modifico views.py

--- ecommerce/GestionUsuarios/views.py
+++ ecommerce/GestionUsuarios/views.py
@@ -9,11 +9,12 @@ def registerUser(request):
     if request.method == 'POST':  # Verificamos que el método de la solicitud sea POST
         #print(request.body.decode('utf-8')) #sin la aclaracion em mostraria solo bits
         try:
+            # Cargar los datos desde el cuerpo de la solicitud
             data = json.loads(request.body.decode('utf-8'))
-
-            mail =  request.POST.get('register_email')   # obtenemso el crudo del html xD
-            contraseña = request.POST.get('register_password') 
-            confirmarContraseña=request.POST.get('confirm_password')
+            # Obtener los valores desde el diccionario `data`
+            mail = data.get('register_email')  
+            contraseña = data.get('register_password') 
+            confirmarContraseña = data.get('confirm_password')
 
             if contraseña != confirmarContraseña:
                 return JsonResponse({'success': False, 'message': 'Las contraseñas no coinciden.'}, status=409)
@@ -28,6 +29,8 @@ def registerUser(request):
             #print(data)  # Cargamos los datos del cuerpo de la solicitud en formato JSON
         except json.JSONDecodeError:
             return JsonResponse({'success': False, 'message': 'Datos JSON inválidos.'}, status=400)
+
+    return render(request,'GestionUsuarios/registro.html')
 '''
         if contraseña != confirmarContraseña:
             #return JsonResponse({'success': False, 'message': 'Contraseña incorrecta.'}, status=409)
@@ -48,7 +51,7 @@ def registerUser(request):
 
     # Si no es un método POST, devolvemos un error 405 (Method Not Allowed)
 '''   
-return render(request,'GestionUsuarios/registro.html')
+
 
 
 
